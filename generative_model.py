ca623cf87c63e0fc255e6d8295c3b585642ecf48
cheesesnakes <shawn27.dsouza@pm.me>
2025-05-06 17:56:03 +0530

    heirachical generative model

--- generative_model.py
+++ generative_model.py
@@ -10,6 +10,7 @@ def _():
     import numpy as np
     from scipy.stats import bernoulli as bern
     from scipy.stats import dirichlet
+    from scipy.stats import beta
     from scipy.special import softmax
     from random import choices
     import matplotlib.pyplot as plt
@@ -17,294 +18,379 @@ def _():
     import pandas as pd
 
     sns.set_theme(style="white", palette="pastel")
-    return bern, choices, dirichlet, mo, np, pd, plt, sns, softmax
+    return bern, beta, choices, dirichlet, mo, np, pd, plt, sns, softmax
 
 
 @app.cell
-def _():
-    states = [1, 2, 3]  # fear, hunger, rest
-    M = [0, 1]  # protection status
-    Q = [
-        0,
-        1,
-        2,
-        3,
-    ]  # treatments: 0: negative control, 1: positive control, 2: baracuda, 3: grouper
+def _(bern, beta, np, pd):
+    # experiment definition
 
-    # transition between states logit(p(s_i | s_{i-1}, M, Q)) = p(s_i | s_{i-1}) + beta_m + beta_q
+    n_protection = 2
+    n_treatment = 4
+    n_ind = 20
+    n_rep = 5
 
-    T_s = [[0.0, 0.2, 0.8], [0.5, 0.0, 0.5], [0.5, 0.5, 0.0]]
+    # treatment and protection levels
 
-    # beta_m = [0, 0.5]
-    beta_m = [0, 0]
+    plot_id = list(range(1, (n_protection * n_treatment * n_rep) + 1))
+    protection = [p for i in range((n_treatment * n_rep)) for p in range(n_protection)]
+    treatment = [t for t in range(n_treatment) for i in range(n_rep) for p in range(n_protection)]
+    replicate = [r for r in range(n_rep) for i in range(n_treatment * n_protection)]
 
-    # beta_q = [0.0, 0.01, 1, 0.5]  # negative control, positive control, baracuda, grouper
-    beta_q = [
-        0.0,
-        0.0,
-        0.0,
-        0.0,
-    ]  # negative control, positive control, baracuda, grouper
+    # covariates
 
-    # duration in each state
 
-    alpha_s = [2.0, 2.0, 2.0]  # shape: variance
-    theta_s = [1.0, 1.0, 1.0]  # scale: mean
+    def predator(protection):
+        """
+        Simulate a predator covariate.
+        """
+        if protection == 0:
+            return bern.rvs(0.3, size=1)
+        else:
+            return bern.rvs(0.75, size=1)
 
-    behaviours = [1, 2, 3]  # foraging, vigilance, moving <-- emmission
 
-    # p(b_i | s_i) states x behaviours
+    def rugosity(protection):
+        """
+        Simulate a rugosity covariate.
+        """
 
-    p_b = [[0.1, 0.8, 0.1], [0.8, 0.1, 0.1], [0.3, 0.3, 0.4]]
+        D_max = 190
 
-    # duration in each behaviour
+        if protection == 0:
+            # less rugose
 
-    alpha_b = [2.0, 2.0, 2.0]  # shape: variance
-    theta_b = [1.0, 1.0, 1.0]  # scale: mean
+            a = np.random.normal(150, 10, size=1)
+        else:
+            # more rugose
+            a = np.random.normal(100, 20, size=1)
+
+        b = D_max - a
+
+        return beta.rvs(a, b, size=1)
+
+
+    def resource(protection):
+        """
+        Simulate a resource covariate.
+        """
+
+        points = 100
+
+        if protection == 0:
+            # more resource
+            a = np.random.normal(20, 2, size=1)
+        else:
+            # less resource
+            a = np.random.normal(10, 2, size=1)
+
+        b = points - a
+
+        return beta.rvs(a, b, size=1)
+
+
+    def unobserved(predator):
+        """
+        Simulate an unobserved covariate affecting energy through predator presence.
+        """
+
+        if predator == 0:
+            return np.random.normal(0, 1, size=1)
+        else:
+            return np.random.normal(1, 1, size=1)
+
+
+    predator = [predator(p) for p in protection]
+    predator = np.array(predator).flatten()
+    rugosity = [rugosity(p) for p in protection]
+    rugosity = np.array(rugosity).flatten()
+    resource = [resource(p) for p in protection]
+    resource = np.array(resource).flatten()
+    unobs_pred = [unobserved(predator[i]) for i in range(len(predator))]
+    unobs_pred = np.array(unobs_pred).flatten()
+
+    # predictor dataframe
+
+    predictor_df = pd.DataFrame(
+        {
+            "plot_id": plot_id,
+            "protection": protection,
+            "treatment": treatment,
+            "replicate": replicate,
+            "predator": predator,
+            "rugosity": rugosity,
+            "resource": resource,
+            "unobs_pred": unobs_pred,
+        }
+    )
+
+    predictor_df
     return (
-        M,
-        Q,
-        T_s,
-        alpha_b,
-        alpha_s,
-        behaviours,
-        beta_m,
-        beta_q,
-        p_b,
-        states,
-        theta_b,
-        theta_s,
+        n_ind,
+        n_protection,
+        n_rep,
+        n_treatment,
+        plot_id,
+        predator,
+        predictor_df,
+        protection,
+        replicate,
+        resource,
+        rugosity,
+        treatment,
+        unobs_pred,
+        unobserved,
     )
 
 
 @app.cell
-def _(
-    M,
-    Q,
-    T_s,
-    alpha_b,
-    alpha_s,
-    behaviours,
-    beta_m,
-    beta_q,
-    np,
-    p_b,
-    softmax,
-    states,
-    theta_b,
-    theta_s,
-):
-    # Simulation
-
-    T = 120.0  # max duration
-    dt = 0.1
-    i = 100  # number of individuals
-
-    # outputs
-    m = np.random.choice(M, size=i)
-    q = np.random.choice(Q, size=i)
-    x_i = [[] for _ in range(i)]
-    t_x_i = [[] for _ in range(i)]
-    b_i = [[] for _ in range(i)]
-    t_b_i = [[] for _ in range(i)]
-
-    for ind in range(i):
-        # initial state
-        p_x = [0.4, 0.3, 0.3]
-        x_t = np.random.choice(states, p=p_x)
-
-        tao = 0.0
-
-        while tao < T:
-            x_i[ind].append(x_t)
-
-            # how much time does the individual spend in this state (x_t)
-            d_x_t = np.random.gamma(alpha_s[x_t - 1], theta_s[x_t - 1])
-            end_s = tao + d_x_t
-            t_x_i[ind].append(min(tao, 120))
-
-            # make emission
-
-            tao_b = tao
-
-            while tao_b < end_s:
-                # pick behaviour
-                p_b_t = p_b[x_t - 1]
-
-                b_t = np.random.choice(behaviours, p=p_b_t)
-                b_i[ind].append(b_t)
-
-                # how much time does the individual spend in this behaviour (b_t)
-                d_b_t = np.random.gamma(alpha_b[x_t - 1], theta_b[x_t - 1])
-                end = tao_b + d_b_t
-                t_b_i[ind].append(min(end, 120))
-
-                # update time
-                tao_b = end
-
-            # pick next state
-
-            p_x_t = softmax(np.log(T_s[x_t - 1]) + beta_m[m[ind]] + beta_q[q[ind]])
-
-            x_t = np.random.choice(states, p=p_x_t)
-
-            # update time
-
-            tao = end_s
-    return (
-        T,
-        b_i,
-        b_t,
-        d_b_t,
-        d_x_t,
-        dt,
-        end,
-        end_s,
-        i,
-        ind,
-        m,
-        p_b_t,
-        p_x,
-        p_x_t,
-        q,
-        t_b_i,
-        t_x_i,
-        tao,
-        tao_b,
-        x_i,
-        x_t,
-    )
-
-
-@app.cell
-def _(i, np, plt, sns, t_x_i, x_i):
-    plt.figure(figsize=(12, 5))  # Create one figure
-
-    sample = np.random.choice(i, size=5)
-
-    for indi in sample:
-        sns.lineplot(
-            x=t_x_i[indi],
-            y=x_i[indi],
-            drawstyle="steps-post",
-            label=f"{indi + 1}",
-            linewidth=2,
-        )
-
-    plt.xlim(0, 120)
-    plt.xlabel("Time")
-    plt.ylabel("State")
-    plt.yticks([1, 2, 3], ["Fear", "Hunger", "Rest"])
-    plt.legend(title="Individual")
-    plt.title("State Trajectories per Individual")
-    plt.tight_layout()
-    plt.show()
-    return indi, sample
-
-
-@app.cell
-def _(b_i, plt, sample, sns, t_b_i):
-    plt.figure(figsize=(12, 5))  # Create one figure
-
-    for _ in sample:
-        sns.lineplot(
-            x=t_b_i[_], y=b_i[_], drawstyle="steps-post", label=f"{_ + 1}", linewidth=2
-        )
-
-    plt.xlim(0, 120)
-    plt.xlabel("Time")
-    plt.ylabel("Behaviour")
-    plt.yticks([1, 2, 3], ["Foraging", "Vigilance", "Moving"])
-    plt.legend(title="Individual")
-    plt.title("Behaviour Trajectories per Individual")
+def _(plt, predictor_df, sns):
+    plt.figure(figsize=(18, 6))
+    plt.subplot(1, 3, 1)
+    sns.histplot(data=predictor_df, x="predator", hue="protection", kde=True)
+    plt.title("Predator Presence")
+    plt.legend(title="Protection", labels=["Outside", "Inside"])
+    plt.subplot(1, 3, 2)
+    sns.histplot(data=predictor_df, x="rugosity", hue="protection", kde=True)
+    plt.title("Rugosity")
+    plt.legend(title="Protection", labels=["Outside", "Inside"])
+    plt.subplot(1, 3, 3)
+    sns.histplot(data=predictor_df, x="resource", hue="protection", kde=True)
+    plt.title("Resource Availability")
+    plt.legend(title="Protection", labels=["Outside", "Inside"])
     plt.tight_layout()
     plt.show()
     return
 
 
 @app.cell
-def _(i, pd, q, t_x_i, x_i):
-    # generate data
+def _(n_ind, np, pd, plot_id, predictor_df):
+    # unobserved variables
 
-    def state_to_df():
-        data = pd.DataFrame()
 
-        for ind in range(i):
-            d = pd.DataFrame(
-                {
-                    "ind_id": ind,
-                    "treatment": q[ind],
-                    "state": x_i[ind],
-                    "start_time": t_x_i[ind],
-                }
-            )
+    def energy(resource, unobserved):
+        """
+        Simulate an energy covariate.
+        """
 
-            data = pd.concat([data, d], ignore_index=True)
+        beta = 10
 
-        data["duration"] = data["start_time"].diff().shift(-1)
+        beta_unobserved = -1
 
-        data.loc[data["duration"] < 0, "duration"] += 120
-        return data
+        alpha = 2
 
-    state_df = state_to_df()
+        eta = beta * resource + beta_unobserved * unobserved
 
-    state_df = state_df.groupby(["ind_id", "state"]).agg({"duration": "sum"})
+        mu = np.exp(eta)
 
-    state_df["prob"] = state_df["duration"] / 120
-    return state_df, state_to_df
+        sigma = np.random.exponential(1)
+
+        shape = (mu / sigma) ** 2
+        scale = sigma**2 / mu
+
+        return np.random.gamma(shape, scale, size=1)
+
+
+    def risk(rugosity, predator, treatment):
+        """
+        Simulate a risk covariate.
+        """
+
+        beta_rugosity = -0.5
+
+        beta_predator = 2
+
+        beta_treatment = [0, 0, 1, 2]
+
+        eta = (beta_rugosity * rugosity) + (beta_predator * predator) + beta_treatment[treatment - 1]
+
+        mu = np.exp(eta)
+
+        sigma = np.random.exponential(1)
+
+        shape = (mu / sigma) ** 2
+        scale = sigma**2 / mu
+
+        return np.random.gamma(shape, scale, size=1)
+
+
+    indiviudals = list(range(1, (len(plot_id) * n_ind) + 1))
+    plot_ind = [plot for i in range(n_ind) for plot in plot_id]
+
+    energy = [
+        energy(
+            predictor_df.loc[predictor_df["plot_id"] == i, "resource"].iloc[0],
+            predictor_df.loc[predictor_df["plot_id"] == i, "unobs_pred"].iloc[0],
+        )
+        for i in plot_ind
+    ]
+    energy = np.array(energy).flatten()
+
+    risk = [
+        risk(
+            predictor_df.loc[predictor_df["plot_id"] == i, "rugosity"].iloc[0],
+            predictor_df.loc[predictor_df["plot_id"] == i, "predator"].iloc[0],
+            predictor_df.loc[predictor_df["plot_id"] == i, "treatment"].iloc[0],
+        )
+        for i in plot_ind
+    ]
+    risk = np.array(risk).flatten()
+
+    # unobserved variables dataframe
+    unobserved_df = pd.DataFrame(
+        {
+            "ind_id": indiviudals,
+            "plot_id": plot_ind,
+            "energy": energy,
+            "risk": risk,
+        }
+    )
+
+    unobserved_df = unobserved_df.merge(predictor_df, on="plot_id")
+    unobserved_df
+    return energy, indiviudals, plot_ind, risk, unobserved_df
 
 
 @app.cell
-def _(plt, sns, state_df):
-    sns.violinplot(data=state_df, x="state", y="prob", hue="state")
-    plt.xticks([0, 1, 2], ["Fear", "Huger", "Rest"])
-    plt.xlabel("State")
-    plt.ylabel("Probability")
-    plt.legend(title="State")
+def _(plt, sns, unobserved_df):
+    plt.figure(figsize=(12, 6))
+    plt.subplot(1, 2, 1)
+    sns.histplot(data=unobserved_df, x="energy", hue="protection", kde=True)
+    plt.title("Energy")
+    plt.legend(title="Protection", labels=["Outside", "Inside"])
+    plt.subplot(1, 2, 2)
+    sns.histplot(data=unobserved_df, x="risk", hue="protection", kde=True)
+    plt.title("Risk")
+    plt.legend(title="Protection", labels=["Outside", "Inside"])
+    plt.tight_layout()
     plt.show()
     return
 
 
 @app.cell
-def _(b_i, i, pd, q, t_b_i):
-    # generate data
+def _(indiviudals, np, pd, plot_ind, predictor_df, unobserved_df):
+    # response variable
 
-    def generate_data():
-        data = pd.DataFrame()
 
-        for ind in range(i):
-            d = pd.DataFrame(
-                {
-                    "ind_id": ind,
-                    "treatment": q[ind],
-                    "behaviour": b_i[ind],
-                    "start_time": t_b_i[ind],
-                }
-            )
+    def foraging(energy, risk):
+        """
+        Simulate a behavioural response.
+        """
 
-            data = pd.concat([data, d], ignore_index=True)
+        beta_energy = -0.2
 
-        return data
+        beta_risk = -0.2
 
-    data = generate_data()
-    return data, generate_data
+        alpha = 2
+
+        eta = alpha + beta_energy * energy + beta_risk * risk
+
+        mu = np.exp(eta)
+
+        sigma = np.random.exponential(1)
+
+        shape = (mu / sigma) ** 2
+        scale = sigma**2 / mu
+
+        return np.random.gamma(shape, scale, size=1)
+
+
+    def vigilance(energy, risk):
+        """
+        Simulate a behavioural response.
+        """
+
+        beta_energy = 0.01
+
+        beta_risk = 0.1
+
+        alpha = 1
+
+        eta = alpha + beta_energy * energy + beta_risk * risk
+
+        mu = np.exp(eta)
+
+        sigma = np.random.exponential(1)
+
+        if mu < 0:
+            mu = 0
+
+        shape = (mu / sigma) ** 2
+        scale = sigma**2 / mu
+
+        return np.random.gamma(shape, scale, size=1)
+
+
+    def movement(energy, risk):
+        """
+        Simulate a behavioural response.
+        """
+
+        beta_energy = 0.091
+
+        beta_risk = -1
+
+        alpha = 1
+
+        eta = alpha + beta_energy * energy + beta_risk * risk
+
+        mu = np.exp(eta)
+
+        sigma = np.random.exponential(1)
+
+        shape = (mu / sigma) ** 2
+        scale = sigma**2 / mu
+
+        return np.random.gamma(shape, scale, size=1)
+
+
+    foraging = [foraging(row["energy"], row["risk"]) for _, row in unobserved_df.iterrows()]
+    foraging = np.array(foraging).flatten()
+    vigilance = [vigilance(row["energy"], row["risk"]) for _, row in unobserved_df.iterrows()]
+    vigilance = np.array(vigilance).flatten()
+    movement = [movement(row["energy"], row["risk"]) for _, row in unobserved_df.iterrows()]
+    movement = np.array(movement).flatten()
+
+    # response dataframe
+    response_df = pd.DataFrame(
+        {
+            "ind_id": indiviudals,
+            "plot_id": plot_ind,
+            "foraging": foraging,
+            "vigilance": vigilance,
+            "movement": movement,
+        }
+    )
+
+    response_df = response_df.merge(predictor_df, on="plot_id")
+
+    response_df
+    return foraging, movement, response_df, vigilance
 
 
 @app.cell
-def _(data):
-    data
+def _(plt, response_df, sns):
+    plt.figure(figsize=(18, 6))
+    plt.subplot(1, 3, 1)
+    sns.histplot(data=response_df, x="foraging", hue="protection", kde=True)
+    plt.title("Foraging")
+    plt.legend(title="Protection", labels=["Outside", "Inside"])
+    plt.subplot(1, 3, 2)
+    sns.histplot(data=response_df, x="vigilance", hue="protection", kde=True)
+    plt.title("Vigilance")
+    plt.legend(title="Protection", labels=["Outside", "Inside"])
+    plt.subplot(1, 3, 3)
+    sns.histplot(data=response_df, x="movement", hue="protection", kde=True)
+    plt.title("Movement")
+    plt.legend(title="Protection", labels=["Outside", "Inside"])
+    plt.tight_layout()
+    plt.show()
     return
 
 
 @app.cell
-def _(data):
-    data.describe(include="all")
-    return
-
-
-@app.cell
-def _(data):
-    data.to_csv("outputs/generated_data.csv")
+def _(response_df):
+    response_df.to_csv("outputs/generated_data.csv", index=False)
     return
 
 

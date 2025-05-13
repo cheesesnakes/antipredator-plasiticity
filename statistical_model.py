5ad90908be1a11e6704a446b4941057f558f8a7e
cheesesnakes <shawn27.dsouza@pm.me>
2025-05-13 10:14:06 +0530

    improved counterfactual plot

--- statistical_model.py
+++ statistical_model.py
@@ -1,34 +1,24 @@
-import marimo
+from cmdstanpy import CmdStanModel
 
-__generated_with = "0.13.6"
-app = marimo.App(width="medium")
+# from cmdstanpy import install_cmdstan
+import pandas as pd
+import arviz as az
+import matplotlib.pyplot as plt
+import seaborn as sns
+import os
+
+# Install CmdStan if not already installed
+# install_cmdstan()
+
+# set the plot style
+sns.set_theme(style="white", palette="pastel")
+az.style.use("arviz-whitegrid")
+
+# load the data
 
 
-@app.cell
-def _():
-    import marimo as mo
-
-    from cmdstanpy import CmdStanModel
-
-    # from cmdstanpy import install_cmdstan
-
-    import pandas as pd
-    import numpy as np
-    import arviz as az
-    import matplotlib.pyplot as plt
-    import seaborn as sns
-    import os
-
-    # Install CmdStan if not already installed
-    # install_cmdstan()
-
-    sns.set_theme(style="white", palette="pastel")
-    # ArviZ ships with style sheets!
-    az.style.use("arviz-whitegrid")
-
-    status = "test"
-
-    if status == "test":
+def load_data(model="test"):
+    if model == "test":
         dirs = [
             "outputs/generated_data_predictors.csv",
             "outputs/generated_data_response.csv",
@@ -42,36 +32,14 @@ def _():
             "outputs/model/data/",
             "outputs/rugosity_raw.csv",
         ]
-    return CmdStanModel, az, dirs, mo, os, pd, plt
+
+    return dirs
 
 
-@app.cell
-def _(dirs, pd):
-    predictors = pd.read_csv(dirs[0])
-
-    predictors
-    return (predictors,)
+# Prepare Stan data
 
 
-@app.cell
-def _(dirs, pd):
-    response = pd.read_csv(dirs[1])
-
-    response
-    return (response,)
-
-
-@app.cell
-def _(dirs, pd):
-    rugosity = pd.read_csv(dirs[3])
-
-    rugosity
-    return (rugosity,)
-
-
-@app.cell
-def _(predictors, response, rugosity):
-    # Prepare Stan data
+def prep_stan(predictors, response, rugosity):
     stan_data = {
         "N": len(response),
         "T": len(predictors["treatment"].unique()),
@@ -87,223 +55,206 @@ def _(predictors, response, rugosity):
         "treatment": predictors["treatment"].values + 1,
         "biomass": predictors["biomass"].values,
     }
-    return (stan_data,)
+
+    return stan_data
 
 
-@app.cell
-def _(CmdStanModel):
-    # stan model
+def load_model(response, output_dir):
+    print("Loading samples from existing files...")
 
+    obs = az.from_dict(
+        observed_data={
+            "D_obs": response["vigilance"].values,
+            "bites_obs": response["bites"].values,
+        }
+    )
+
+    model_data = az.from_cmdstan(
+        output_dir + "*.csv",
+        posterior_predictive=[
+            "D_pred",
+            "bites_pred",
+            "D_cf",
+            "bites_cf",
+        ],
+    )
+
+    model_data = az.concat(model_data, obs)
+
+    return model_data
+
+
+def run_model(output_dir, stan_data, response, chains=4):
+    print("Running model...")
+
+    # compile stan model
+    print("Compiling model...")
     model = CmdStanModel(stan_file="model.stan")
-    return (model,)
 
+    # delete model_files
+    print("Running model...")
 
-@app.cell
-def _(az, dirs, model, os, response, stan_data):
-    run = False
-    chains = 4
+    for file in os.listdir(output_dir):
+        os.remove(output_dir + file)
 
-    output_dir = dirs[2]
-
-    if not os.path.exists(output_dir):
-        os.makedirs(output_dir)
-
-    # check if csv file exists
-
-    model_files = []
-
-    for filename in os.listdir(output_dir):
-        if filename.endswith(".csv"):
-            model_files.append(filename)
-
-    if not run and len(model_files) == chains:
-        print("Loading samples from existing files...")
-
-        obs = az.from_dict(
-            observed_data={
-                "D_obs": response["vigilance"].values,
-                "bites_obs": response["bites"].values,
-            }
-        )
-
-        model_data = az.from_cmdstan(
-            output_dir + "*.csv",
-            posterior_predictive=[
-                "D_pred",
-                "bites_pred",
-                "D_cf",
-                "bites_cf",
-            ],
-        )
-
-        model_data = az.concat(model_data, obs)
-
-    else:
-        print("Running model...")
-
-        # delete model_files
-        for file in os.listdir(output_dir):
-            os.remove(output_dir + file)
-
-        fit = model.sample(
-            data=stan_data,
-            chains=chains,
-            iter_warmup=1000,
-            iter_sampling=1000,
-            seed=123,
-            parallel_chains=4,
-            threads_per_chain=8,
-            output_dir=output_dir,
-            # max_treedepth=15,
-            # adapt_delta=0.99,
-        )
-
-        obs = az.from_dict(
-            observed_data={
-                "D_obs": response["vigilance"].values,
-                "bites_obs": response["bites"].values,
-            }
-        )
-        model_data = az.from_cmdstanpy(
-            fit,
-            posterior_predictive=[
-                "D_pred",
-                "bites_pred",
-                "D_cf",
-                "bites_cf",
-            ],
-        )
-        model_data = az.concat(model_data, obs)
-    return fit, model_data, run
-
-
-@app.cell
-def _(fit, mo, run):
-    if run:
-        mo.md(fit.diagnose())  # if divergence is observed
-    return
-
-
-@app.cell
-def _(az, model_data):
-    az.summary(model_data, hdi_prob=0.95, round_to=2)
-    return
-
-
-@app.cell
-def _(az, model_data):
-    az.plot_trace(model_data, compact=False, var_names=["beta_res"], figsize=(16, 6))
-    return
-
-
-@app.cell
-def _(az, model_data):
-    az.plot_trace(
-        model_data, compact=False, var_names=["beta_treatment"], figsize=(16, 6)
+    fit = model.sample(
+        data=stan_data,
+        chains=chains,
+        iter_warmup=1000,
+        iter_sampling=1000,
+        seed=123,
+        parallel_chains=4,
+        threads_per_chain=8,
+        output_dir=output_dir,
+        # max_treedepth=15,
+        # adapt_delta=0.99,
     )
-    return
 
+    print("Model run complete.")
 
-@app.cell
-def _(az, model_data):
-    az.plot_trace(model_data, compact=False, var_names=["beta_rug"], figsize=(16, 6))
-    return
+    print("Diagnosing model...")
 
+    print(fit.diagnose())
 
-@app.cell
-def _(az, model_data):
-    az.plot_trace(
-        model_data, compact=False, var_names=["beta_predator"], figsize=(16, 6)
+    # make arviz data
+    print("Creating ArviZ data...")
+
+    obs = az.from_dict(
+        observed_data={
+            "D_obs": response["vigilance"].values,
+            "bites_obs": response["bites"].values,
+        }
     )
-    return
+
+    model_data = az.from_cmdstanpy(
+        fit,
+        posterior_predictive=[
+            "D_pred",
+            "bites_pred",
+            "D_cf",
+            "bites_cf",
+        ],
+    )
+
+    model_data = az.concat(model_data, obs)
+
+    return model_data
 
 
-@app.cell
-def _(az, model_data):
-    az.plot_trace(model_data, compact=False, var_names=["beta_risk"], figsize=(16, 6))
-    return
+def diagnostic_plots(model_data):
+    # traceplot
+    print("Creating traceplot...")
+    az.plot_trace(
+        model_data,
+        compact=False,
+        var_names=["beta_risk", "beta_pi_risk", "beta_risk_bites", "beta_rug"],
+        figsize=(16, 24),
+    )
 
+    plt.savefig("figures/generative/traceplot.png", dpi=300)
 
-@app.cell
-def _(az, model_data, plt):
+    # posterior predictive check
+    print("Creating posterior predictive check...")
     az.plot_ppc(
         model_data,
         kind="cumulative",
         data_pairs={"D_obs": "D_pred", "bites_obs": "bites_pred"},
         figsize=(10, 6),
     )
-    plt.tight_layout()
-    plt.show()
-    return
+    plt.savefig("figures/generative/ppc.png", dpi=300)
 
 
-@app.cell
-def _(mo, model_data):
-    posterior = model_data.posterior
-    n_chains = posterior.sizes["chain"]
-    n_draw = posterior.sizes["draw"]
-    n_samples = n_chains * n_draw
+def counterfactual_treatments(model_data):
+    print("Computing counterfactual treatments...")
+    # counterfactual analysis
 
-    mo.md(f"Total number of samples = {n_samples}")
-    return
-
-
-@app.cell
-def _(az, model_data):
     D_cf = az.extract(model_data.posterior_predictive, var_names="D_cf")
 
     D_cf = D_cf.values
 
-    Diff_negative_positive = D_cf[1,] - D_cf[0,]
+    Diff_01 = (D_cf[1,] - D_cf[0,]).mean(axis=2)
 
-    Diff_negative_positive = Diff_negative_positive.mean(axis=2)
+    Diff_02 = (D_cf[2,] - D_cf[0,]).mean(axis=2)
 
-    Diff_negative_1 = D_cf[2,] - D_cf[0,]
+    Diff_03 = (D_cf[3,] - D_cf[0,]).mean(axis=2)
 
-    Diff_negative_1 = Diff_negative_1.mean(axis=2)
+    print("Creating counterfactual plots...")
 
-    Diff_negative_2 = D_cf[3,] - D_cf[0,]
+    behavior = ["Foraging", "Vigilance", "Movement"]
 
-    Diff_negative_2 = Diff_negative_2.mean(axis=2)
-    return Diff_negative_1, Diff_negative_2, Diff_negative_positive
-
-
-@app.cell
-def _(Diff_negative_1, Diff_negative_2, Diff_negative_positive, az, plt):
-    # Combine posteriors
-
-    fig, ax1 = plt.subplots(1, 3, figsize=(18, 6))
-    ax1 = ax1.flatten()
+    plt.figure(figsize=(24, 6))
 
     for b in range(3):
         posterior_data = {
-            "Positive Control": Diff_negative_positive[b, :],
-            "Treatment 1": Diff_negative_1[b, :],
-            "Treatment 2": Diff_negative_2[b, :],
+            "Positive Control": Diff_01[b, :],
+            "Treatment 1": Diff_02[b, :],
+            "Treatment 2": Diff_03[b, :],
         }
 
-        # Create forest plot
-        az.plot_forest(
-            posterior_data,
-            kind="forestplot",  # Classic forest plot style
-            combined=True,  # Treat each array as one group
-            hdi_prob=0.87,  # 87% HDI shown by default
-            quartiles=True,  # Adds 50% interval inside 87%
-            markersize=8,
-            linewidth=4,
-            colors="black",
-            ax=ax1[b],
+        posterior_df = pd.DataFrame(posterior_data).melt(
+            var_name="Treatment", value_name="Effect"
         )
 
-    plt.tight_layout()
-    plt.show()
-    return
+        plt.subplot(1, 3, b + 1)
+        plt.title(f"{behavior[b]}")
+        sns.violinplot(
+            x="Treatment",
+            y="Effect",
+            data=posterior_df,
+            linewidth=1,
+            hue="Treatment",
+            palette="pastel",
+            alpha=0.5,
+        )
+        # plt.ylim(-0.2, 0.2)
+
+    plt.savefig("figures/generative/counterfactual_treatment.png", dpi=300)
 
 
-@app.cell
-def _():
-    return
+def main(run=False, chains=4):
+    # load data
+
+    print("Loading data...")
+    dirs = load_data(model="test")
+    predictors = pd.read_csv(dirs[0])
+    response = pd.read_csv(dirs[1])
+    rugosity = pd.read_csv(dirs[3])
+
+    # prepare data
+    stan_data = prep_stan(predictors, response, rugosity)
+
+    # run model or load existing model
+
+    output_dir = dirs[2]
+
+    print("Checking for existing model files...")
+
+    if os.path.exists(output_dir):
+        # check if csv file exists
+
+        model_files = []
+
+        for filename in os.listdir(output_dir):
+            if filename.endswith(".csv"):
+                model_files.append(filename)
+    else:
+        os.makedirs(output_dir)
+        model_files = []
+
+    if not run and len(model_files) == chains:
+        model_data = load_model(response, output_dir)
+    else:
+        model_data = run_model(output_dir, stan_data, response, chains=chains)
+
+    # diagnostic plots
+
+    diagnostic_plots(model_data)
+
+    # counterfactual treatments
+    counterfactual_treatments(model_data)
+
+    return 0
 
 
 if __name__ == "__main__":
-    app.run()
+    main(run=True, chains=4)
